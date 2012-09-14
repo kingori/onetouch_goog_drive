1d311de63bb37f3a7798e0af901cd0286c0e297d
sewon ann <kingori@gmail.com>
2012-09-14 21:30:35 +0900

    gen, bin, out 제거

--- lib_actionbar_sherlock/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/kingori/dev/workspace/eclipse/Episode-Android/lib_actionbar_sherlock/bin/AndroidManifest.xml : \
-/Users/kingori/dev/workspace/eclipse/Episode-Android/lib_actionbar_sherlock/AndroidManifest.xml \
